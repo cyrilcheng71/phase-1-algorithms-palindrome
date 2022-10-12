71f6ec7d29985380ceacd2dc44788b99d5e7e04b
Man Cheng <cyrilcheng@protonmail.com>
2022-10-11 21:34:44 -0700

    commit tree f612b14cf385c88402abb2bd14055c9f38e7c073

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let wordSplit = word.split("");
+
+  let reserveWord =[];
+  wordSplit.map((word) => {
+    reserveWord.unshift(word);
+  });
+
+  if (word === reserveWord.join("")){
+    return true;
+  } else { 
+    return false;
+  }}
+
 
 /* 
   Add your pseudocode here
 */
-
+//break down the word to letters
+//split string into array
+//then compare
 /*
   Add written explanation of your solution here
 */
+//use split method to turn the string to letters
+//create new array and insert them with unshit
+//then compare the string with reserver word to the original string to get the true or false result
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
